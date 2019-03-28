9a59e186e54e5eae6403216d3961dcd5577c6142
Yihua Zhang <michaelzhang2@msn.com>
2019-03-28 14:47:25 +0100

    add react-router-dom and add new HatsPage, then provide routes for Home and HatsPage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage';
+import HatsPage from './pages/hats/hats';
 
 import './App.css';
 
@@ -8,7 +10,10 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <HomePage />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/hats' component={HatsPage} />
+        </Switch>
       </div>
     );
   }
---
+++ src/pages/hats/hats.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class HatsPage extends Component {
+  render() {
+    return <div>Hats Page</div>;
+  }
+}
+
+export default HatsPage;
