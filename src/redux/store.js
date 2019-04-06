61acf68643bfbfe2b57bd02d291647f725159c72
Yihua Zhang <michaelzhang2@msn.com>
2019-04-06 23:15:59 +0200

    using redux-persist to persist the cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from 'redux-persist';
+
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
@@ -6,6 +8,8 @@ import rootReducer from './root-reducer';
 
 const middlewares = [logger, thunk];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
