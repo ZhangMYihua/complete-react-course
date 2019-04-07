f16d2d31fd69aa7806e662f286bcf0a1f6f68dbd
Yihua Zhang <michaelzhang2@msn.com>
2019-04-07 20:44:08 +0200

    switching cart-item reducer to use new item type

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -4,18 +4,13 @@ export const getCartItems = state => state.cart.cartItems;
 
 export const getCartHidden = state => state.cart.hidden;
 
+export const getCombinedCartPrices = createSelector(
+  getCartItems,
+  items => items.reduce((total, item) => total + item.price * item.quantity, 0)
+);
+
 export const getCartItemsCount = createSelector(
   getCartItems,
   cartItems =>
     cartItems.reduce((count, cartItem) => cartItem.quantity + count, 0)
-);
-
-export const getCombinedCartPrices = createSelector(
-  getCartItems,
-  items => items.reduce((total, item) => total + item.price, 0)
-);
-
-export const getCombinedCartPrices = createSelector(
-  getCartItems,
-  items => items.reduce((total, item) => total + item.price, 0)
 );
