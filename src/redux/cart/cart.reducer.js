71635e9f9e430847fc5e70ce662369d4a9c3cc4c
Yihua Zhang <michaelzhang2@msn.com>
2019-04-16 12:52:38 +0200

    updating utils to match newer code

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -12,12 +12,12 @@ export const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...addItem(state.cartItems, action.payload)]
+        cartItems: addItem(state.cartItems, action.payload)
       };
     case CartActionTypes.REMOVE_ITEM:
       return {
         ...state,
-        cartItems: [...removeItem(state.cartItems, action.payload)]
+        cartItems: removeItem(state.cartItems, action.payload)
       };
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -2,21 +2,29 @@ export const addItem = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     cartItem => cartItem.id === cartItemToAdd.id
   );
+
   if (existingCartItem) {
-    existingCartItem.quantity++;
-    return [...cartItems];
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : { ...cartItem }
+    );
   }
+
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
-
-export const removeItem = (cartItems, cartItemToAdd) => {
+export const removeItem = (cartItems, cartItemToRemoveId) => {
   const existingCartItem = cartItems.find(
-    cartItem => cartItem.id === cartItemToAdd.id
+    cartItem => cartItem.id === cartItemToRemoveId
   );
-  if (existingCartItem.quantity > 1) {
-    existingCartItem.quantity--;
-    return [...cartItems];
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemoveId);
   }
 
-  return [...cartItems.filter(cartItem => cartItem.id !== cartItemToAdd.id)];
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemoveId
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : { ...cartItem }
+  );
 };
