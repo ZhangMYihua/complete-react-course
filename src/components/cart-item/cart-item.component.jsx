9b0989827474ea38ddc517c006e9502237d5f501
Yihua Zhang <michaelzhang2@msn.com>
2019-04-25 14:47:20 +0200

    updating cart-item and checkout-item

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
-import './cart-item.styles.scss';
+import { CartItemContainer, ItemDetailsContainer } from './cart-item.styles';
 
 const CartItem = ({ cartItem: { imageUrl, price, name, quantity } }) => {
   return (
-    <div className='cart-item'>
+    <CartItemContainer className='cart-item'>
       <img src={imageUrl} alt='item' />
-      <div className='item-details'>
-        <span className='name'>{name}</span>
-        <span className='price'>{`${quantity} x $${price}`}</span>
-      </div>
-    </div>
+      <ItemDetailsContainer className='item-details'>
+        <span>{name}</span>
+        <span>{`${quantity} x $${price}`}</span>
+      </ItemDetailsContainer>
+    </CartItemContainer>
   );
 };
 
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -7,30 +7,32 @@ import {
   removeItemFromCart
 } from '../../redux/cart/cart.actions';
 
-import './checkout-item.styles.scss';
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  TextContainer,
+  QuantityContainer,
+  RemoveItemContainer
+} from './checkout-item.styles';
 
 const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
   const { imageUrl, price, name, quantity, id } = cartItem;
   return (
-    <div className='checkout-item'>
-      <div className='image-container'>
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt='item' />
-      </div>
-      <span className='name'>{name}</span>
-      <div className='quantity'>
-        <div className='arrow' onClick={() => removeItem(id)}>
-          &#10094;
-        </div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={() => addItem(cartItem)}>
-          &#10095;
-        </div>
-      </div>
-      <span className='price'>${price}</span>
-      <div className='remove-button' onClick={() => clearItem(id)}>
+      </ImageContainer>
+      <TextContainer>{name}</TextContainer>
+      <QuantityContainer>
+        <div onClick={() => removeItem(id)}>&#10094;</div>
+        <span>{quantity}</span>
+        <div onClick={() => addItem(cartItem)}>&#10095;</div>
+      </QuantityContainer>
+      <TextContainer>${price}</TextContainer>
+      <RemoveItemContainer onClick={() => clearItem(id)}>
         &#x2715;
-      </div>
-    </div>
+      </RemoveItemContainer>
+    </CheckoutItemContainer>
   );
 };
 
